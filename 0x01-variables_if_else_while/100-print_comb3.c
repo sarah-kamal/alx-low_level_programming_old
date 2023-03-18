e43db1fc17193017aefe0e4b70d95bfc6fcf1e3d
sara <sarah.kamal.soliman@gmail.com>
2023-03-18 12:19:44 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,13 +17,13 @@ int main(void)
 	for ( ; d1 <= 8; )
 	{
 		putchar(d1);
-		for (d2 = d1 + 1; d2 < 9; d2++)
+		for (d2 = d1 + 1; d2 <= 9; d2++)
 			putchar(d2);
 		if (d1 == 8 && d2 == 9)
 			break;
 		putchar(',');
 		putchar(' ');
-		ch++;
+		d1++;
 	}
 	putchar('\n');
 	return (0);
