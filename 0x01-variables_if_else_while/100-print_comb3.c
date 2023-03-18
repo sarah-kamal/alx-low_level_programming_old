9510697a5aebe28a5f4bc08d53e67a787e86178f
sara <sarah.kamal.soliman@gmail.com>
2023-03-18 12:15:09 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+  * main - Entery point
+  *
+  * Return: Always 0
+  *
+  */
+
+int main(void)
+{
+	int d1;
+	int d2;
+
+	d1 = 0;
+
+	for ( ; d1 <= 8; )
+	{
+		putchar(ch);
+		for (d2 = d1 + 1; d2 < 9; d2++)
+			putchar(d2);
+		if (d1 == 8 && d2 == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
