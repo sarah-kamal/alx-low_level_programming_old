00b1a52c7fe80113b29ca125b3468886d184fc20
sara <sarah.kamal.soliman@gmail.com>
2023-03-18 11:26:41 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+  * main - Entery point
+  *
+  * Return: Always 0
+  *
+  */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	for ( ; ch <= 'z'; )
+	{
+		if (ch == 'e' || ch == 'q')
+			continue;
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
