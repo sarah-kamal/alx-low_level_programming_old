c6bed2a864427436fbc412c399a794995f925bc8
sara <sarah.kamal.soliman@gmail.com>
2023-03-18 11:11:32 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+
+/**
+  * main - Entery point
+  *
+  * Return: Always 0
+  *
+  */
+
+int main(void)
+{
+	char ch;
+
+	ch = a;
+
+	for (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	return (0);
+}
