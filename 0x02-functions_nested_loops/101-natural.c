a61ed61751e353a65b49322281bb4dec88fc279b
Your Name <you@example.com>
2023-03-21 12:59:41 -0700

    l

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int u;
+	int sum;
+
+	sum = 0;
+	for (u = 0; u < 1024; u++)
+	{
+		if (u % 3 == 0 || u % 5 == 0)
+			sum += u;
+	}
+	puchar(u + '0');
+}
