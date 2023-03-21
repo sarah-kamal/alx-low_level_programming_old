f526fa71374e342f1baf311805b2313a877301ec
Your Name <you@example.com>
2023-03-21 04:52:42 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,23 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
+ * _putchar_t
  *
  * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+void _putchar(void)
 {
-	printf("_putchar\n");
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    //return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,14 @@
 #include<stdio.h>
-#include<putchar.c>
+#include<0-putchar.c>
 /**
  * main - Entry point
  *
  * Description: 'the program's description'
  * Return : always 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	_putchar();
 	return 0;
 }
+
