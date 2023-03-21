4075816a6751de9b8dd2457c6d8f94185b98eb79
Your Name <you@example.com>
2023-03-21 09:26:35 -0700

    task 4

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,21 +7,18 @@
  * @c (int)
  * Return : 1 (lower) , 0 otherwise
  */
-int print_sign(int c)
+int _abs(int c)
 {
 	if (c > 0)
 	{
-		putchar('+');
-		return (1);
+		return (c);
 	}
 	else if (c < 0)
 	{
-		putchar('-');
-		return (-1);
+		return (-1 * c);
 	}
 	else
 	{
-		putchar('0');
 		return (0);
 	}
 
