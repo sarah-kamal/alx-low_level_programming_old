05d4a5342f753172fd1f151fa3f74769476601b1
Your Name <you@example.com>
2023-03-21 03:28:30 -0700

    l

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<putchar.c>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return : always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	 _putchar('p');
+	  _putchar('u');
+	   _putchar('t');
+	    _putchar('c');
+	    _putchar('h');
+	     _putchar('a');
+	  _putchar('r');
+}
