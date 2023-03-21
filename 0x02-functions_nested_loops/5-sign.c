7098554b0c63589e9ad64a056766a2147e4aacff
Your Name <you@example.com>
2023-03-21 09:21:22 -0700

    task 4

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *int print_sign - checks if low
+ * Description : checks if char is lower
+ * @c (int)
+ * Return : 1 (lower) , 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (c < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else
+	{
+		puchar('0');
+		return (0);
+	}
+
+}
