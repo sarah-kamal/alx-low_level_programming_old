eb892c12b1f6aca02f21a2f6c9cc7aeeb6ce3abf
Your Name <you@example.com>
2023-03-21 08:04:18 -0700

    l

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include"main.h"
 
 /**
- * _putchar_t
+ * main-:Entery point
  *
  * Description: 'the program's description'
  *
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
