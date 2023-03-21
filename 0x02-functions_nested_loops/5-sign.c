817b4b06b7f0f414c0f0891324af238b6c6e7393
Your Name <you@example.com>
2023-03-21 09:29:20 -0700

    task 4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (c < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 
