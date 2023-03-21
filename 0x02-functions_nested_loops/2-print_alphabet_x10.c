8bd61a83d59e596c4eeead2b67a8e97374d45828
Your Name <you@example.com>
2023-03-21 08:51:47 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"main.h"
+#include"_putchar.c"
+/**
+ *
+ print_alphabet_x10 - Enterypoint
+ * Description: prints all lowercase  followed by a new line.
+ *
+ * Return : 0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int count;
+
+	count = 1;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	 	_putchar('\n');
+		if (ch == 'z' && count <= 10)
+		{
+			ch = 'a';
+			count++;
+		}
+	}
+
+}
