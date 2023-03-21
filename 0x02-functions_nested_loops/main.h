a57eaf6f871dd195575b73327989d6e4d02dc376
Your Name <you@example.com>
2023-03-21 03:30:19 -0700

    l

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,11 +10,12 @@
 int main(void)
 {
 	_putchar('_');
-	 _putchar('p');
-	  _putchar('u');
-	   _putchar('t');
-	    _putchar('c');
-	    _putchar('h');
-	     _putchar('a');
-	  _putchar('r');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return 0;
 }
