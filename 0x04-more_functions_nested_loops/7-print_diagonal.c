3b527182f6beeca5432e9c7551009c95a6db2eae
Your Name <you@example.com>
2023-03-23 13:38:28 -0700

    l

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,25 +7,27 @@
  */
 void print_diagonal(int n)
 {
+	int i;
+	int j;
 	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
-	} else
+	}
+	else
 	{
-		int i, j;
-
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 					_putchar('\\');
-				else if (j < i)
+				else 
 					_putchar(' ');
 			}
 			_putchar('\n');
-			return;
+			
 		}
 	}
+	return;
 }
