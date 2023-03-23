513a04cf7dec3900ca11979ad3b3b4cc8e3095b7
Your Name <you@example.com>
2023-03-23 14:21:42 -0700

    l

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int _putchar(char c);
+void print_square(int size);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
