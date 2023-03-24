17162cdc84e0c2ed722a9772869ee9dae6ca335b
Your Name <you@example.com>
2023-03-24 06:13:43 -0700

    task 1,2 updated

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,10 @@
 #include"main.h"
 
 /**
-  * int _isupper -returns if character is upper case or not
-  *
-  * Description: checks if a char is uppercase
-  *
-  * Return: 1 in uppercase ,  otherwise
-  */
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c > 64 && c < 91)
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,13 @@
 #include"main.h"
 
 /**
-  * _isdigit-Returns if character is upper case or not
-  *
-  * Description: checks if a char is uppercase
-  *
-  * Return: 1 in uppercase ,  otherwise
-  */
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
-	if (c > 47 && c < 58)
+	if (c >= 0 && c <= 9)
 		return (1);
 	else
 		return (0);
