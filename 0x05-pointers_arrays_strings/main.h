9d2f51f325947f3f1a4002d06b191ebc786f0dce
Your Name <you@example.com>
2023-03-27 06:47:51 -0700

    k

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
-void puts2(char *str);
 int _strlen(char *s);
 void reset_to_98(int *n);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void swap_int(int *a, int *b);
+void puts2(char *str);
