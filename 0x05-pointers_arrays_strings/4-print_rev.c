20dc66ea36e4931d4d78545bf25e4d2188b4b775
Your Name <you@example.com>
2023-03-27 04:30:49 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+
+	while (i > 0)
+	{
+		s--;
+		_putchar(*s);
+		i--;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@ int _strlen(char *s);
 void reset_to_98(int *n);
 void _puts(char *str);
 int _putchar(char c);
+void print_rev(char *s);
 void swap_int(int *a, int *b);
