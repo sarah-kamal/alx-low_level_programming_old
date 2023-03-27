02c875e42b472fb960a5d4ab08b99738b0376f0a
Your Name <you@example.com>
2023-03-27 03:45:09 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include<iostream>
+/**
+ * reset_to_98 - resets the value of an integer pointed to by n to 98
+ *
+ * @n: a pointer to an integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	  *n = 98;
+}
+
