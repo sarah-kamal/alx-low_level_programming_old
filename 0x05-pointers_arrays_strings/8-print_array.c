bc02b14f4e99510b209df7d1883ded5ddae1996d
Your Name <you@example.com>
2023-03-27 16:16:24 -0700

    kk

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,14 +7,14 @@
  * @n: int to check
  * Return: 0 is success
  */
- void print_array(int *a, int n)
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-		if (i != n-1)
+		if (i != n - 1)
 			printf("%d, ",a[i]);
 		else
-			printf("%d",a[i]);
-	printf( "\n");
+			printf("%d", a[i]);
+	printf("\n");
 }
