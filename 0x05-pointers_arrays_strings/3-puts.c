34a14921081460f5912b9945081dfcae9c5f13a7
Your Name <you@example.com>
2023-03-27 04:09:42 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * _puts - Prints a string to stdout
+ * @str: Pointer to string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 int _strlen(char *s);
 void reset_to_98(int *n);
+void _puts(char *str);
+int _putchar(char c);
 void swap_int(int *a, int *b);
