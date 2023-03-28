cceb3c3e19fe18e750aad90c23b150f6e4cc263c
Your Name <you@example.com>
2023-03-28 13:12:05 -0700

    hj

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,6 @@
-#include "main.h"
-#include<stdio.h>
-#include<math.h>
-
-/**
+#include <stdio.h>
+#include"main.h"
+/*
  * _atoi - Convert a string to an integer.
  * @s: The pointer to convert
  *
@@ -10,66 +8,69 @@
  */
 int _atoi(char *s)
 {
-	int isneg;
-	int cont;
-	int numfound;
-	int index;
-	int count;
-	int c;
-	int number;
-	int n;
+        int isneg;
+        int cont;
+        int l;
+        int io;
+        int numfound;
+        int index;
+        int count;
+        int c;
+        int number;
+        int n;
 
-	c = 0;
-	isneg = 0;
-	numfound = 0;
-	cont = 0;
-	count = 0;
-	while (s[c] != '\0')
-	{
-		if (s[c] == '-')
-			isneg = 1;
-		else if (s[c] == '+')
-			isneg = 0;
-		if (s[c] > 47 && s[c] < 58)
-		{
-			if (! numfound)
-			{
-				numfound = 1;
-				cont = 1;
-				index = c;
-				count++;
-			}
-			else if (cont)
-				count++;
-		}
-		else
-		{
-			cont = 0;
-		}
-		c++;
-	}
-	if (numfound)
-	{
-		for ( ; index < index + count; index++)
-		{
-			n = s[index] - '0';
-			for (c = 0; c < count - 1; c++)
-				n *= 10;
-			number += n;
-		}
-		if (isneg)
-			number *= -1;
-		return (number);
-	}
-	else
-	{
-		return (0);
-	}
+        c = 0;
+        isneg = 0;
+        numfound = 0;
+        cont = 0;
+        count = 0;
+        while (s[c] != '\0')
+        {
+                if (s[c] == '-')
+                        isneg = 1;
+                else if (s[c] == '+')
+                        isneg = 0;
+                if (s[c] > 47 && s[c] < 58)
+                {
+                        if (! numfound)
+                        {
+                                numfound = 1;
+                                cont = 1;
+                                index = c;
+                                count++;
+                        }
+                        else 
+                        if (cont)
+                                count++;
+                }
+                else
+                {
+                        cont = 0;
+                }
+                c++;
+        }
+        if (numfound)
+        {
+            l = count;
+            io = index;
+                while (index < io + count)
+                {
+                        n = s[index] - '0';
+                        for (c = 0; c < l - 1; c++)
+                        {
+                                n *= 10;
+                        }
+                        l--;
+                        number += n;
+                        index++;
+                }
+                if (isneg)
+                        number *= -1;
+                return (number);
+        }
+        else
+        {
+                return (0);
+        }
 
 }
-
-
-
-
-
-
