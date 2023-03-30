d0eb6d8f6314bb647f8971fcca5d5f589c6dddb8
Your Name <you@example.com>
2023-03-30 06:01:47 -0700

    lol

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,12 +15,14 @@ char *cap_string(char *s)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		for (j = 0; j < 13;j++)
+		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == str[j])
+			if (s[i] == str[j] || i == 0)
 			{
 			if (s[i + 1] != '\0' && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				s[i + 1] -= 32;
+			else if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
 			}
 		}
 		i++;
