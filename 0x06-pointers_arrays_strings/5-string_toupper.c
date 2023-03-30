a43ca812b0e0ccf59f04b9e051e799473dc83a1f
Your Name <you@example.com>
2023-03-30 05:37:14 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #include<stdio.h>
+char *string_toupper(char *s);
 void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
