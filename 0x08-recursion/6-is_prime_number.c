b77a260fd7be401a995b3b1a19b15dafedc404cb
Your Name <you@example.com>
2023-04-05 06:43:14 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include"main.h"
+int isprime(int, int);
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
+{
+	if (n <= 0)
+		return 0;
+	return (isprime(n, n/2 + 1));
+}
+/**
+ * isprime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int isprime(int n, int i)
+{
+	if (i == 1)
+		return 1;
+	if (n % i == 0)
+		return 0;
+	return (isprime(n, i - 1));
+}
