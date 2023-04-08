376beee500008f62711412c3d0d63be2ba3d1e45
Your Name <you@example.com>
2023-04-08 06:43:57 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+  * main - function printing num of args
+  * @argc: nom of args
+  * @argv: array of strings
+  * Return: 0 upon success
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
