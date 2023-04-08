3d6a3efc695920eaeaf16ae787cf74be782412d2
Your Name <you@example.com>
2023-04-08 06:40:01 -0700

    opop

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - a programme that prints itsname followed by new line
+ * @argc: number of args
+ * @argv: narray of args
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
