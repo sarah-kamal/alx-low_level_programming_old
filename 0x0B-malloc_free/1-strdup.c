8bb9dacc708f2b022ea57b7e0a1e082fcffac55c
Your Name <you@example.com>
2023-04-11 18:25:06 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include"main.h"
+/**
+  *_strdup - returns str
+  *@str: string
+  *Return: array of char
+  */
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *p;
+
+	i = 0;
+	if (str)
+	{
+		while(str[i] != '\0')
+			i++;
+	}
+	if (i = 0 || !str)
+		return (NULL);
+	p = (char *) malloc(sizeof(char)*i);
+	if(!p)
+		return (NULL);
+	len = i;
+	for (i = 0; i < len ;i++)
+	{
+		p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
 #include<stdlib.h>
 #include<stdio.h>
+char *_strdup(char *str);
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
