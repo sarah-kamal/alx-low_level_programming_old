811734cbab513b8ccd1f8e19a64757fa9c4bc182
Your Name <you@example.com>
2023-04-11 18:12:58 -0700

    l

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,12 @@ char *create_array(unsigned int size, char c)
 {
 	char *p;
 	unsigned int i;
-	
+
 	if (size <= 0)
 		return (NULL);
-	p = (char*)malloc(sizeof(char) * size);
+	p = (char *)malloc(sizeof(char) * size);
+	if (!p)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
