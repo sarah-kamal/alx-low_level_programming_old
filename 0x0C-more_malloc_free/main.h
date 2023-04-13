e4d11802edd5af4e605f0b5910e58f640207c8b7
Your Name <you@example.com>
2023-04-13 11:13:27 -0700

    2

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,4 +2,5 @@
 #include<stdlib.h>
 #include<string.h>
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 void *malloc_checked(unsigned int b);
