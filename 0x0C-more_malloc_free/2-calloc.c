04ed5cb65b43d851f1c791b554ec55421c509485
Your Name <you@example.com>
2023-04-13 11:12:51 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	return (ptr);
+}
