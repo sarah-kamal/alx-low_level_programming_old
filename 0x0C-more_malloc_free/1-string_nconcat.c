42749bcdeb9a6439ab9818402cf051b7f944e368
Your Name <you@example.com>
2023-04-13 11:07:12 -0700

    p

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 	if (n > strlen(s2))
 		n = strlen(s2);
 	ptr = malloc(strlen(s1) + n + 1);
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < strlen(s1); i++)
 		ptr[i] = s1[i];
-	for (k = i; k < n; k++)
+	for (k = i; k < n + strlen(s1); k++)
 		ptr[k] = s2[k - strlen(s1)];
 	ptr[k] = '\0';
 	return (ptr);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
