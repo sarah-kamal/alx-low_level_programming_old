01eb0b4e0ece469520391c732886269e9f2e3f18
Your Name <you@example.com>
2023-04-17 15:31:48 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,47 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	char *namee, *ownerr;
+	dog_t *doggy;
+
+	if (name == NULL || char == NULL)
+		return (NULL);
+	namee = malloc(sizeof(name) + 1);
+	if (!namee)
+		return (NULL);
+	ownerr = malloc(sizeof(owner) + 1);
+	if (!ownerr)
+		return (NULL);
+	doggy = malloc(sizeof(dog_t));
+	if (!doggy)
+		return (NULL);
+	i = 0;
+	while (name[i] != '\0')
+	{
+		namee[i] = name[i];
+		i++;
+	}
+	namee[i] = '\0';
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		ownerr[i] = owner[i];
+		i++;
+	}
+	ownerr[i] = '\0';
+	doggy->name = namee;
+	doggy->age = age;
+	doggy->owner = ownerr;
+	return (doggy);
+}
