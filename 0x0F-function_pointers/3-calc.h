548fd636c8733ecabf97f1f071fa885f0fe8c8fb
Your Name <you@example.com>
2023-04-20 15:18:13 -0700

    ppp

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef _3_CALC_H
+#define _3_CALC_H
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -9,8 +11,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -18,3 +20,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int (*get_op_func(char *s))(int, int);
 int op_mod(int a, int b);
+#endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,7 @@ int main(int argc, char *argv[])
 {
 	int a;
 	int b;
-	int (*func) (int, int);
+	int(*func) (int, int);
 
 	if (argc < 4)
 	{
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	func = get_op_func(argv[2]);
-	if(!func)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(98);
@@ -24,5 +24,5 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	printf("%d\n", func(a, b));
-	return(func(a, b));
+	return (func(a, b));
 }
