e7a6bb3aa1e4dccd354f8245117a92fdb052c77e
Your Name <you@example.com>
2023-04-20 15:07:28 -0700

    3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include"3-calc.h"
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int (*func) (int, int);
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+	if(!func)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((func == op_div || func == op_mod) && b  == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", func(a, b));
+	return(func(a, b));
+}
