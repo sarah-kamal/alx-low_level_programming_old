c2e12686867357bcef68c6b1e2556e67dc01f733
Your Name <you@example.com>
2023-04-20 15:24:38 -0700

    lll

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,19 @@
 #include"3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
 int main(int argc, char *argv[])
 {
 	int a;
 	int b;
-	int(*func) (int, int);
+	int (*func)(int, int);
 
 	if (argc < 4)
 	{
