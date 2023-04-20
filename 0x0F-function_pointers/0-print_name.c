13dca950c2c7e038d861b6b8f14fdd58877b7116
Your Name <you@example.com>
2023-04-20 13:28:41 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of name
+ *
+ * Rteurn: void
+ */
+void print_name(char *name, void (*f)(char *v))
+{
+	if (v != NULL && f != NULL)
+	print(s);
+}
