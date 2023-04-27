80bebcfb66249bae34dee4c85e9db0a26c2a4864
Your Name <you@example.com>
2023-04-27 11:32:49 -0700

    o

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,16 +6,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head)
+	if (!head)
+		return ;
+	free_list(head->next);
+	if (head->next == NULL)
 	{
-		if (head -> next)
-			free_list(head->next);
-		if (head->next == NULL)
-		{
-			if (head->str)
-				free(str);
-			free(head);
-			head = NULL;
-		}
+		if (head->str)
+			free(head->str);
+		free(head);
 	}
 }
