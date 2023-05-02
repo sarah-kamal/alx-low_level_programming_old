978ad4a83262bfc13b843022177d8683a1c8f3a3
Your Name <you@example.com>
2023-05-02 04:24:04 -0700

    task 7

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,7 @@
  * pop_listint - Removes the head node of a linked list and returns its data.
  * @head: Pointer to the head of the linked list.
  *
- * Return: The data of the removed head node as an integer, or 0 if the list is empty.
+ * Return: The data of , or 0 if the list is empty.
  */
 int pop_listint(listint_t **head)
 {
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include"lists.h"
+/**
+ * get_nodeint_at_index - Returns the node at a given index in a linked list.
+ * @head: Pointer to the head of the linked list.
+ * @index: Index of the node to return.
+ *
+ * Return: Pointer to the node at the specified index, or NULL if the index is out of range.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	while (head && i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	if (i == index)
+		return (head);
+	else
+		return (NULL);
+}
+
