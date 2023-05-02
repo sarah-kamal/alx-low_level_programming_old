89e7927fc8b7ea7b1e80827eeb033cd71b03fef3
Your Name <you@example.com>
2023-05-02 03:49:19 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+/**
+ * free_listint - Frees a linked list and its memory.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Return: Nothing.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head ->next;
+		free(temp);
+		temp = NULL;
+	}
+}
+
