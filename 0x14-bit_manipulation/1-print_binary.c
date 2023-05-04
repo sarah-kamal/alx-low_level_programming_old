b4f134c3a5cd68e347430b2ac0bdcd4d6f4c9248
Your Name <you@example.com>
2023-05-04 16:16:47 -0700

    0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,15 +2,22 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask;
+	int flag;
 
-	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	while (mask > 0)
+	flag = 0;
+	mask = 1UL << (sizeof(n) * 8 - 1);
+	while (mask > 0 )
 	{
 		if (n & mask)
-			_putchar('0');
-		else
+		{
 			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+			_putchar('0');
 		mask >>= 1;
 	}
+	if (!flag)
+		_putchar('0');
 }
 
