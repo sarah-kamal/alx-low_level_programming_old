611d957f2f7fc2d0a82a3913adc55c93724e70d8
Your Name <you@example.com>
2023-05-04 15:04:10 -0700

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-int _putchar(char c)
+int _putchar(char c);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
