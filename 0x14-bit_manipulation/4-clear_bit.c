4f4397a18be46b7affbdbe93ee97a3380cdd272c
Your Name <you@example.com>
2023-05-04 16:59:42 -0700

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,11 @@
 #include"main.h"
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-        return -1;
+	unsigned long int mask;
 
-    // Use a mask to clear the bit at the given index
-    unsigned long int mask = ~(1UL << index);
-    *n &= mask;
-
-    return 1;
+	if (index >= sizeof(unsigned long int) * 8)
+        	return -1;
+	mask =~ (1UL << index);
+	*n &= mask; 
+	return 1;
 }
