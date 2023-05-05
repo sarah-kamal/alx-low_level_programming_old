418aba8568d439c35b8cd3570fac7dc954a64480
Your Name <you@example.com>
2023-05-04 17:07:08 -0700

    l

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,14 @@
 #include"main.h"
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (n ^ m);
+	unsigned int mask,counter;
+
+	mask = n ^ m;
+	counter = 0;
+	while (mask > 0)
+	{
+		counter += 1 & mask;
+		mask >>= 1;
+	}
+	return (counter);
 }
