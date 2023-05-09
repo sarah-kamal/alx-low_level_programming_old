699f38726c0f5af427591c63a4a7523df59e5543
Your Name <you@example.com>
2023-05-08 17:08:04 -0700

    l

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,4 +4,5 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
