1edeb7ac77de7dd27f38e2e242ceb2762b05aec6
Your Name <you@example.com>
2023-05-12 13:48:34 -0700

    minor adj

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN
+#define MAIN
 #include<string.h>
 #include<stdio.h>
 #include<unistd.h>
@@ -10,3 +12,4 @@
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+#endif
